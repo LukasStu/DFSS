5d1502a2f7e00939755d53b382d0579ad53d62ba
LukasStu <72215368+LukasStu@users.noreply.github.com>
2020-10-26 09:34:17 +0100

    Update Konfidenz_Intervall.py

--- Eigenes/Konfidenz_Intervall.py
+++ Eigenes/Konfidenz_Intervall.py
@@ -13,6 +13,9 @@ a = np.array([1,2,3,4,4,4,5,5,5,5,4,4,4,6,7,8])
 x_quer, s = np.mean(a), np.std(a)
 conf_int1 = stats.norm.interval(0.68, loc=x_quer, scale=s/np.sqrt(len(a)))
 
+#print('{:0.2%} of the single draws are in conf_int_a'
+#      .format(((a >= conf_int_a[0]) & (a < conf_int_a[1])).sum() / float(N)))
+
 # Hier Standartnormalverteilung implizit
 u_quer = 5.0
 sigma = np.sqrt(0.09)
